0c947b7ade83ab5eb1c5119ef09320bf0d51e428
sergioviniciuss <sergioviniciuss@gmail.com>
2018-06-10 20:34:42 +0200

    Building a simple form using formik

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { withFormik } from 'formik';
+import Yup from 'yup';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const App = ({
+    values,
+    handleChange,
+    handleSubmit
+}) => (
+    <form onSubmit={handleSubmit}>
+        <input type="email" name="email" placeholder="Email" value={values.email} onChange={handleChange}/>
+        <input type="password" name="password" placeholder="Password" value={values.password} onChange={handleChange}/>
+        <button>Submit</button>
+    </form>
+)
+
+const FormikApp = withFormik({
+    mapPropsToValues({email, password}){
+        return {
+            email: email || '',
+            password: password || ''
+        }
+    },
+    handleSubmit(values) {
+        console.log(values)
+    }
+    
+})(App)
+
+
+ReactDOM.render(<FormikApp />, document.getElementById('root'));
